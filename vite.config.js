a0a68977453b04691251f9f9021364f823729bbd
Polina Zhukovskaya <mayabessonova_16_94@mail.ru>
2023-10-03 13:48:40 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import { externalizeDeps } from 'vite-plugin-externalize-deps'
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { externalizeDeps } from "vite-plugin-externalize-deps";
 
 export default defineConfig({
-  plugins: [react(),
-    nodePolyfills(), externalizeDeps()],
+  plugins: [react(), externalizeDeps(), nodePolyfills()],
   build: {
-    outDir: 'dist'
+    outDir: "dist",
   },
   base: "/Mts-tariff",
-  });
+});
